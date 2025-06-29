5ea401069b1d2a83278afdbe17b1011d50958f0a
anastasiyanasvyazi <anastasnasvyazi@yandex.com>
2025-06-29 18:38:17 +0300

    Add files via upload

---
+++ domashka.cy.js
@@ -0,0 +1,79 @@
+describe('Авторизация - Позитивный сценарий', function () {
+    beforeEach(function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').should('be.visible'); // Проверка загрузки формы
+    });
+
+
+    it('Верный пароль и верный логин', function () {
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('not.be.disabled').click();
+        cy.get('#messageHeader').should('be.visible')
+            .and('contain', 'Авторизация прошла успешно');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    });
+});
+
+describe('Восстановление пароля', function () {
+    beforeEach(function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#forgotEmailButton').should('be.visible'); // Проверка загрузки формы
+    });
+
+    it('Успешное восстановление пароля', function () {
+        cy.get('#forgotEmailButton').click();
+        cy.get('#mailForgot').type('german@dolnikov.ru');
+        cy.get('#restoreEmailButton').click();
+        cy.get('#messageHeader').should('be.visible')
+            .and('contain', 'Успешно отправили пароль на e-mail');
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible');
+    });
+});
+
+describe('Авторизация - Негативные и позитивные сценарии', function () {
+    beforeEach(function () {
+        cy.visit('https://login.qa.studio/');
+        cy.get('#mail').should('be.visible'); // Проверка загрузки формы
+    });
+
+
+    it('Негативный кейс: Правильный логин, неверный пароль', function () {
+        cy.get('#mail').type('german@dolnikov.ru'); // Правильный логин
+        cy.get('#pass').type('wrongPassword'); // Неверный пароль
+        cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
+        cy.get('#messageHeader').should('be.visible')
+            .and('contain', 'Такого логина или пароля нет'); // Проверка текста ошибки
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Проверка кнопки крестика
+    });
+
+    it('Негативный кейс: Неверный логин, правильный пароль', function () {
+    cy.get('#mail').type('invalid@dolnikov.ru'); // Неверный логин
+    cy.get('#pass').type('iLoveqastudio1'); // Правильный пароль
+    cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
+    cy.get('#messageHeader', { timeout: 10000 }).should('be.visible')
+        .invoke('text').then((text) => {
+            cy.log('Сообщение об ошибке: ' + text); // Логируем текст для анализа
+        });
+    cy.get('#messageHeader').should('be.visible')
+        .and('contain', 'Такого логина или пароля нет'); // Ожидаемое сообщение
+    cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Проверка кнопки крестика
+});
+
+    it('Негативный кейс: Логин без @, правильный пароль', function () {
+        cy.get('#mail').type('germandolnikov.ru'); // Логин без @
+        cy.get('#pass').type('iLoveqastudio1'); // Правильный пароль
+        cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
+        cy.get('#messageHeader').should('be.visible')
+            .and('contain', 'Нужно исправить проблему валидации'); // Проверка текста ошибки
+    });
+
+    it('Позитивный кейс: Приведение логина к строчным буквам', function () {
+        cy.get('#mail').type('GerMan@Dolnikov.ru'); // Логин с разным регистром
+        cy.get('#pass').type('iLoveqastudio1'); // Правильный пароль
+        cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
+        cy.get('#messageHeader').should('be.visible')
+            .and('contain', 'Такого логина или пароля нет'); // 
+        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Проверка кнопки крестика
+    });
+});
---
+++ pokemon.cy.js
@@ -0,0 +1,22 @@
+describe('Проверка покупки нового аватара', function () {                 // название набора тестов
+    it('e2e тест на покупку нового аватара для тренера', function () {   // название теста
+         cy.visit('https://pokemonbattle.ru/');                          // переходим на сайт https://pokemonbattle.ru/
+         cy.get('input[id="k_email"]').type('USER_LOGIN');                   // вводим логин
+         cy.get('input[id="k_password"]').type('USER_PASSWORD');               // вводим пароль
+         cy.get('button[type="submit"]').click();                // нажимаем кнопку Подтвердить
+         cy.wait(2000);
+         cy.get('.header_card_trainer').click();            // Клик в шапке на аву тренера
+         cy.wait(2000);
+         cy.get('.k_mobile > :nth-child(5) > #dropdown > img').click(); // нажимаем кнопку Смена аватара
+         cy.get('.available > button').first().click();   // кликаем Купить у первого доступного аватара
+         cy.wait(4000);
+         cy.get('.card_number').type('4303911599022955');                     // вводим номер карты
+         cy.get('.card_csv').type('504');                             // вводим CVV карты
+         cy.get('.card_date').type('0733');                           // вводим срок действия карты
+         cy.get('.card_name').type('Eleanor Vosper');                         // вводим имя владельца действия карты
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();     // нажимаем кнопку Оплатить
+         cy.get('.threeds_number').type('6056');                            // вводим код подтверждения СМС
+         cy.get('.style_1_base_button_payment_body > .style_1_base_button_payment').click();   // нажимаем кнопку Оплатить
+         cy.contains('Покупка прошла успешно').should('be.visible');     // проверяем наличие и видимость сообщения об успешной покупке
+     });
+ });
