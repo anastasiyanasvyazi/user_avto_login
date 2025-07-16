b653af16e3a28cff54117b7a1a4c74d414c69ee7
anastasiyanasvyazi <anastasnasvyazi@yandex.com>
2025-07-16 11:46:33 +0300

    Update domashka.cy.js

--- domashka.cy.js
+++ domashka.cy.js
@@ -4,7 +4,6 @@ describe('Авторизация - Позитивный сценарий', funct
         cy.get('#mail').should('be.visible'); // Проверка загрузки формы
     });
 
-
     it('Верный пароль и верный логин', function () {
         cy.get('#mail').type('german@dolnikov.ru');
         cy.get('#pass').type('iLoveqastudio1');
@@ -37,40 +36,33 @@ describe('Авторизация - Негативные и позитивные 
         cy.get('#mail').should('be.visible'); // Проверка загрузки формы
     });
 
-
     it('Негативный кейс: Правильный логин, неверный пароль', function () {
-        cy.get('#mail').type('german@dolnikov.ru'); // Правильный логин
-        cy.get('#pass').type('wrongPassword'); // Неверный пароль
-        cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
+        cy.get('#mail').type('german@dolnikov.ru');
+        cy.get('#pass').type('wrongPassword');
+        cy.get('#loginButton').should('not.be.disabled').click();
         cy.get('#messageHeader').should('be.visible')
-            .and('contain', 'Такого логина или пароля нет'); // Проверка текста ошибки
-        cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Проверка кнопки крестика
+            .and('contain', 'Такого логина или пароля нет');
     });
 
     it('Негативный кейс: Неверный логин, правильный пароль', function () {
-    cy.get('#mail').type('invalid@dolnikov.ru'); // Неверный логин
-    cy.get('#pass').type('iLoveqastudio1'); // Правильный пароль
-    cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
-    cy.get('#messageHeader', { timeout: 10000 }).should('be.visible')
-        .invoke('text').then((text) => {
-            cy.log('Сообщение об ошибке: ' + text); // Логируем текст для анализа
-        });
-    cy.get('#messageHeader').should('be.visible')
-        .and('contain', 'Такого логина или пароля нет'); // Ожидаемое сообщение
-    cy.get('#exitMessageButton > .exitIcon').should('be.visible'); // Проверка кнопки крестика
-});
+        cy.get('#mail').type('invalid@dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('not.be.disabled').click();
+        cy.get('#messageHeader').should('be.visible')
+            .and('contain', 'Такого логина или пароля нет');
+    });
 
     it('Негативный кейс: Логин без @, правильный пароль', function () {
-        cy.get('#mail').type('germandolnikov.ru'); // Логин без @
-        cy.get('#pass').type('iLoveqastudio1'); // Правильный пароль
-        cy.get('#loginButton').should('not.be.disabled').click(); // Нажать войти
+        cy.get('#mail').type('germandolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
+        cy.get('#loginButton').should('not.be.disabled').click();
         cy.get('#messageHeader').should('be.visible')
-            .and('contain', 'Нужно исправить проблему валидации'); // Проверка текста ошибки
+            .and('contain', 'Нужно исправить проблему валидации');
     });
 
     it('Позитивный кейс: Приведение логина к строчным буквам', function () {
-        cy.get('#mail').type('GerMan@Dolnikov.ru'); // Логин с разным регистром
-        cy.get('#pass').type('iLoveqastudio1'); // Правильный пароль
+        cy.get('#mail').type('GerMan@Dolnikov.ru');
+        cy.get('#pass').type('iLoveqastudio1');
         cy.get('#loginButton').should('not.be.disabled').click();
         cy.get('#messageHeader').should('be.visible')
             .and('contain', 'Авторизация прошла успешно');
